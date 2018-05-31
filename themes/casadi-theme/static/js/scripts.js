171309e2ddf710c9068ed5588e749a58efc5113e
asteinh <armin.steinhauser@kuleuven.be>
2018-05-31 11:39:55 +0200

    doc styling

--- themes/casadi-theme/static/js/scripts.js
+++ themes/casadi-theme/static/js/scripts.js
@@ -2,4 +2,24 @@ $(document).ready(function() {
   $("#btn-search").click(function() {
     $('#field-search').fadeToggle(200);
   });
+
+  // var href = $(this).attr('href');
+  $("a.reference.internal").each(function(i, k) {
+    var id = $(k).attr("href").replace('index.html','');
+    var href = "javascript:scrollTo(\""+id+"\")";
+    // $(k).attr("href", "javascript:void(0);");
+    // href="javascript:void(0)" onclick="location.href='"
+    // $(k).click(function() {
+    //   var tag = $(this)[0];
+    //   scrollTo($(tag).attr("href").replace('index.html',''));
+    //   event.stopPropagation();
+    // });
+    // $(k).attr("href", "");
+    $(k).attr("href", id);
+  });
 });
+
+function scrollTo(id) {
+  console.log(id);
+  $('html,body').animate({scrollTop: $(id).offset().top - 65}, 'slow');
+}
