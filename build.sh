6be4d47cb58ec9b1308b526314eea6723390c999
Joris Gillis <joris.gillis42gmail.com>
2018-08-23 17:20:12 +0200

    What is blocking?

--- build.sh
+++ build.sh
@@ -25,11 +25,11 @@ cd themes/casadi-theme/
 npm update
 
 # run gulp to build static files and collect them for deploying
-gulp --verbose scss
+gulp scss
 
 # run hugo server
 cd ../..
 
-rm -rf public && hugo -v
+rm -rf public && hugo
 
 echo "+ Build script finished."
