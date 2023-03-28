a858436fdf5a01fe2c56e239b8b7a52948ab60fc
Joris Gillis <joris.gillis42@gmail.com>
2023-03-28 12:30:57 +0200

    Updated gulp file by GPT4

--- build_in_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v `pwd`:/local ghcr.io/casadi/web:latest ./build.sh
--- themes/casadi-theme/gulpfile.js
+++ themes/casadi-theme/gulpfile.js
@@ -1,24 +1,24 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename'),
-    minify = require('gulp-minifier');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const minify = require('gulp-minifier');
 
-var config = {
-    staticDir: './static',
-    scssPath: './src/scss',
-}
+const config = {
+  staticDir: './static',
+  scssPath: './src/scss',
+};
 
 // Compile SCSS files to CSS, minified and readable
-gulp.task('scss', function () {
-  gulp.src(config.scssPath + '/**/*.scss')
+function scss() {
+  return gulp.src(config.scssPath + '/**/*.scss')
     .pipe(sass({
       includePaths: [
         config.scssPath
       ]
     }))
     .pipe(autoprefixer({
-      browsers : ['last 20 versions']
+      overrideBrowserslist: ['last 20 versions']
     }))
     .pipe(gulp.dest(config.staticDir + '/_css'))
     .pipe(minify({
@@ -28,13 +28,17 @@ gulp.task('scss', function () {
       minifyCSS: true
     }))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(config.staticDir + '/_css'))
-});
+    .pipe(gulp.dest(config.staticDir + '/_css'));
+}
 
 // Watch asset folder for changes
-gulp.task('watch', ['scss'], function () {
-  gulp.watch(config.scssPath + '/**/*', ['scss'])
-});
+function watch() {
+  gulp.watch(config.scssPath + '/**/*', scss);
+}
 
 // Set watch as default task
-gulp.task('default', ['watch'])
+gulp.task('default', gulp.series(scss, watch));
+
+// Export the tasks
+exports.scss = scss;
+exports.watch = watch;
