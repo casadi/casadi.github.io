d69b4bc7e9ccfb5dec3f0e58c20cadb1e76a598e
Joris Gillis <joris.gillis42@gmail.com>
2023-03-29 22:59:09 +0200

    Towards github actions

--- run_build.sh
+++ run_build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+sudo mkdir -p public
 docker run --rm -v `pwd`:/local ghcr.io/casadi/web:latest ./build.sh
+sudo chmod a+w -R public
+cp -R content/api public
+touch public/.nojekyll # To make sure files with leading _underscore are served on github pages
+echo "web.casadi.org" > CNAME
+cp CNAME public
