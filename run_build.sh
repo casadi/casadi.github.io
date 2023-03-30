b976e2b030f61f7e8ae13df48a14a4ae1f6454c0
jgillis <joris.gillis42@gmail.com>
2023-03-30 22:54:39 +0200

    Update run_build.sh

--- run_build.sh
+++ run_build.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 
 sudo mkdir -p public
 docker run --rm -v `pwd`:/local ghcr.io/casadi/web:latest ./build.sh
-sudo chmod a+w -R public
+sudo chown -R $USER:$USER public
 cp -R content/api public
 touch public/.nojekyll # To make sure files with leading _underscore are served on github pages
 echo "web.casadi.org" > CNAME
