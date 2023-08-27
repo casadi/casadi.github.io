c1fbad5b3905d07bad776d14b0f85cc1280b09df
Joris Gillis <joris.gillis42@gmail.com>
2023-08-27 11:17:46 +0200

    bottom banner

--- _js/scripts.js
+++ _js/scripts.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-  $("a.reference.internal").each(function(i, k) {
-    var id = $(k).attr("href").replace('index.html','');
-    $(k).attr("href", id);
+  $('a.reference.internal').each(function(i, k) {
+    var id = $(k).attr('href').replace('index.html','');
+    $(k).attr('href', id);
   });
 
   $('div.multisnip').each(function(ib, block) {
-    $(block).prepend("<ul class='nav nav-fill nav-tabs-multisnip'></ul>");
+    $(block).prepend('<ul class="nav nav-fill nav-tabs-multisnip"></ul>');
     $(block).children('div.highlight').each(function(ih, highlight) {
       if(ih == 0) $(highlight).show();
       else $(highlight).hide();
       $(highlight).children('pre').children('code').each(function(ic, code) {
-        var lang = $(code).attr("data-lang");
+        var lang = $(code).attr('data-lang');
         if(ih == 0) {
-          var li = $("<li>", {"class": "nav-item isshown"});
+          var li = $('<li>', {'class': 'nav-item isshown'});
         } else {
-          var li = $("<li>", {"class": "nav-item"});
+          var li = $('<li>', {'class': 'nav-item'});
         }
-        if (lang=="octave") {
-          lang = lang + "/Matlab";
+        if (lang=='octave') {
+          lang = lang + '/Matlab';
         }
-        if (lang=="cpp") {
-          lang = "C++";
+        if (lang=='cpp') {
+          lang = 'C++';
         }
-        li.append("<a>" + lang + "</a>");
+        li.append('<a>' + lang + '</a>');
 
         li.click(function(){ makeActive(highlight, li) });
-        $(block).find("ul.nav-tabs-multisnip").append(li);
+        $(block).find('ul.nav-tabs-multisnip').append(li);
       });
     });
   });
@@ -33,11 +33,11 @@ $(document).ready(function() {
   var hash = window.location.hash;
   if(hash) {
     var id = hash.substr(1);
-    var card = $(".card[id="+id+"]")[0];
-    if(typeof card !== "undefined") {
+    var card = $('.card[id="+id+"]')[0];
+    if(typeof card !== 'undefined') {
       $(card).find('a.card-header.collapsed').each(function(i, a) {
         $(a).removeClass('collapsed');
-        $(a).attr("aria-expanded", "true");
+        $(a).attr('aria-expanded', 'true');
       });
       $(card).find('div.collapse').each(function(i, d) {
         $(d).addClass('show');
@@ -49,16 +49,28 @@ $(document).ready(function() {
 });
 
 (function addAnchorElements() {
-  console.log('running addAnchorElements');
+  // console.log('running addAnchorElements');
   $('.content [id]').each(function(i, el) {
     $('<div class="anchor" id="' + el.id + '"></div>').insertBefore(el);
-    $(el).attr("id", el.id + '-sub');
+    $(el).attr('id', el.id + '-sub');
   });
 })();
 
 function makeActive(hl, li) {
   $(hl).parent().children('div.highlight').hide();
   $(hl).show();
-  $(li).parent().children().each(function(i, el) { $(el).removeClass("isshown"); });
-  $(li).addClass("isshown");
+  $(li).parent().children().each(function(i, el) { $(el).removeClass('isshown'); });
+  $(li).addClass('isshown');
 }
+
+let observer = new IntersectionObserver(
+  function(a,b) {
+    if (a[0].isIntersecting) {
+      $('#bottom-banner').fadeTo(250, 0);
+    } else {
+      $('#bottom-banner').fadeTo(250, 1);
+    }
+  }
+);
+
+observer.observe(document.getElementById('landing-cover'));
